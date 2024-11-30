45dee83ed2db1bc834a1b51152ba7e2ab7136ed9
unknown <business.life.bekir@gmail.com>
2024-11-30 07:17:22 +0100

    load mateial

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'] // Korrigiert: "styleUrls" (Plural)
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
